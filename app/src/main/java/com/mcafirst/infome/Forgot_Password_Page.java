907699cf1049dda7dc5d2d3144a1fd13d1da7a5c
nihaltiwari01 <contactnihaltiwari@gmail.com>
2025-09-03 18:00:47 +0530

    first commit

--- app/src/main/java/com/mcafirst/infome/Forgot_Password_Page.java
+++ app/src/main/java/com/mcafirst/infome/Forgot_Password_Page.java
@@ -20,5 +20,6 @@ public class Forgot_Password_Page extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //dj
     }
 }
